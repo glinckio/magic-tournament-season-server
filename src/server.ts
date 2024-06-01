0cd2b1f139852a714ae8cf6f995558d76c3572eb
Gabriel Linck <gabriel.jobc13@gmail.com>
2024-06-01 11:07:23 -0300

    add typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import "reflect-metadata";
 import express from "express";
-
 import { Router } from "express";
 
 const app = express();
-
 const route = Router();
 
 app.use(express.json());
-
 app.use(route);
 
 app.listen(3333, () => "server running on port 3333");
