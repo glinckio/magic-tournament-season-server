0ffca482f9bb4a76bb986bf8be05a8e98ab14de2
Gabriel Linck <gabriel.jobc13@gmail.com>
2024-06-11 09:55:24 -0300

    add DbFindAllTournaments unit tests

---
+++ src/data/usecases/find-all-tournaments/db-find-all-tournaments.spec.ts
@@ -0,0 +1,79 @@
+import { TournamentModel } from "../../../domain/models/tourmanent";
+import { AddTournamentModel } from "../../../domain/usecases/add-tournament";
+import { Tournament } from "../../../infra/db/typeorm/db/entities/tournament-entity";
+import { TournamentRepository } from "../../protocols/db/tournament-repository";
+import { DbFindAllTournaments } from "./db-find-all-tournaments";
+
+const makeFakeTournaments = (): TournamentModel[] => [
+  {
+    id: 1,
+    name: "valid_name",
+    startsAt: "valid_date",
+  },
+  {
+    id: 2,
+    name: "valid_name",
+    startsAt: "valid_date",
+  },
+];
+
+const makeTournamentRepository = (): TournamentRepository => {
+  class DbFindAllTournamentsStub implements TournamentRepository {
+    add(tournament: AddTournamentModel): Promise<Tournament> {
+      throw new Error("Method not implemented.");
+    }
+    findById(id: number): Promise<Tournament> {
+      throw new Error("Method not implemented.");
+    }
+    findByName(name: string): Promise<Tournament> {
+      throw new Error("Method not implemented.");
+    }
+    findAll(): Promise<Tournament[]> {
+      throw new Error("Method not implemented.");
+    }
+    remove(id: number): Promise<null> {
+      throw new Error("Method not implemented.");
+    }
+  }
+
+  return new DbFindAllTournamentsStub();
+};
+
+interface SutTypes {
+  sut: DbFindAllTournaments;
+  tournamentRepository: TournamentRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const tournamentRepository = makeTournamentRepository();
+  const sut = new DbFindAllTournaments(tournamentRepository);
+
+  return {
+    sut,
+    tournamentRepository,
+  };
+};
+
+describe("DbFindAllTournaments", () => {
+  it("should throws of DbFindAllTournaments throws", async () => {
+    const { tournamentRepository, sut } = makeSut();
+    jest
+      .spyOn(tournamentRepository, "findAll")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const promise = sut.findAll();
+    await expect(promise).rejects.toThrow();
+  });
+  it("should return tournament on success", async () => {
+    const { tournamentRepository, sut } = makeSut();
+    jest
+      .spyOn(tournamentRepository, "findAll")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => resolve(makeFakeTournaments()))
+      );
+    const account = await sut.findAll();
+    expect(account).toEqual(makeFakeTournaments());
+  });
+});
--- src/main/factories/find-all-tournaments/make-all-tournaments.ts
+++ src/main/factories/find-all-tournaments/make-all-tournaments.ts
@@ -1,4 +1,4 @@
-import { DbFindAllTournaments } from "../../../data/usecases/find-all-tournaments/find-all-tournaments";
+import { DbFindAllTournaments } from "../../../data/usecases/find-all-tournaments/db-find-all-tournaments";
 import { TournamentTypeOrmRepository } from "../../../infra/db/typeorm/tournament-repository/tournament";
 import { FindAllTournamentsController } from "../../../presentation/controllers/find-all-tornaments/find-all-tournaments";
 import { Controller } from "../../../presentation/protocols/controller";
